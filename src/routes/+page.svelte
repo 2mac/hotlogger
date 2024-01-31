af0d1a44cc47ec26a90ec35f3f9119238834f806
David McMackins II <contact@mcmackins.org>
2024-01-31 06:06:40 -0600

    Add logo to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,25 @@
 </script>
 
 <div class="container-narrow">
-    <h2>Set your call sign</h2>
-
-    <form method="POST" use:enhance>
-        <input type="text" name="callsign" placeholder="Call Sign" value={$page.data.callsign || ''} required/>
-        <button>Sign in</button>
-    </form>
+    <div class="center">
+        <img src="/hotlogger-main.png" alt="HotLogger Logo" />
+        
+        <h2>Set your call sign</h2>
+    
+        <form method="POST" use:enhance>
+            <input type="text" name="callsign" placeholder="Call Sign" value={$page.data.callsign || ''} required/>
+            <button>Sign in</button>
+        </form>
+    </div>
 </div>
+
+<style>
+    img {
+        max-height: 50vh;
+        margin: auto;
+    }
+
+    div.center {
+        text-align: center;
+    }
+</style>
