e6d2019ec49faa224d0e0b3babeb35565fe8aa8b
Sir Gausshawk <sirgausshawk@Sirs-MBP.ht.home>
2024-02-10 14:24:43 -0600

    add new logo to header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,3 @@
-<header>
-    <h1><em>Hot</em>Logger</h1>
-</header>
-
 <main>
     <slot />
 </main>
-
-<style>
-    em {
-        color: firebrick;
-    }
-
-    header {
-        border-bottom: thin solid;
-        margin-bottom: 1em;
-    }
-</style>
