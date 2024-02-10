a5ad975bc9c124edec30f0a1135f1d2f006c57e2
Joe Jackson <joejak93@gmail.com>
2024-02-10 17:11:40 -0600

    wip

---
+++ src/routes/log/[id]/ContactCard.svelte
@@ -0,0 +1,25 @@
+<script>
+   export let contact;
+</script>
+
+<div style="    display: flex;
+flex-direction: column;
+align-items: center;
+border: solid;
+border-color: black;
+border-width: thin;
+border-radius: 4px;
+min-width: 260px;">
+<table>
+    {#each Object.keys(contact) as key}
+    <tr>
+        <td>{key}</td>
+        <td>{contact[key]}</td>
+    </tr>
+    {/each}
+</table>
+</div>
+
+<style>
+
+</style>
