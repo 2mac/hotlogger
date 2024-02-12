e63713ad59f666c7b5f72521286e7e5ac572bdac
Joseph Jackson <joejak93@gmail.com>
2024-02-11 22:32:13 -0600

    basic card layout completed

--- src/routes/log/[id]/ContactCard.svelte
+++ src/routes/log/[id]/ContactCard.svelte
@@ -49,46 +49,119 @@
 </script>
 
 <div
+  id="container"
+  class="main-bg"
   style="    display: flex;
 flex-direction: column;
 align-items: center;
 border: solid;
 border-color: black;
 border-width: thin;
-border-radius: 4px;
+border-radius: .5em;
 min-width: 300px;
-gap: 4px"
+gap: 4px; 
+margin:4px;"
 >
-  <div class="inline-section" style="margin-left:auto; justify-content: space-between;">
+  <div
+    class="inline-section"
+    style="margin-left:auto; justify-content: space-between;"
+  >
     <p style="color: lightgray;">{contact.time}</p>
     <em><b>{timestamp}</b> <cite>ago</cite></em>
   </div>
-  <div style="display:flex; flex-direction:row; align-items:center; gap:4px">
-    <div style="display:flex; flex-grow:1; flex-direction: column; align-items:flex-end">
+  <div
+    style="display:flex; flex-direction:row; align-items:center; gap:4px; width: 100%"
+  >
+    <div
+      style="display: flex;
+    flex-grow: 1;
+    flex-direction: row;
+    align-items: flex-end;
+    border: chartreuse;
+    width: 100%;
+    border-top-right-radius: 0.5em;
+    border-bottom-right-radius: 0.5em;
+    background: chartreuse;
+    padding: 4px; 
+    align-items: center"
+    >
+      <div>
+        <p style="color:darkgreen; font-size:small">sent</p>
+      </div>
+      <div
+        style="display:flex; flex-direction:column; align-items:flex-end; margin-left: auto;"
+      >
         <p>
-            <b>{contact.op_call}</b>
+          <b>{contact.op_call}</b>
         </p>
         <p>
-            <b>{contact.rst_recd}</b>
+          <b>{contact.rst_recd}</b>
         </p>
+      </div>
     </div>
     <div>
-        <p>⇌</p>
-        <p>⇌</p>
+      <p>⇌</p>
+      <p>⇌</p>
     </div>
-    <div style="display:flex; flex-grow:1; flex-direction: column; align-items:flex-start">
+    <div
+      style="display: flex;
+    flex-grow: 1;
+    flex-direction: row;
+    align-items: flex-start;
+    border: moccasin;
+    width: 100%;
+    border-top-left-radius: 0.5em;
+    border-bottom-left-radius: 0.5em;
+    background: moccasin;
+    padding: 4px;
+    align-items: center"
+    >
+      <div
+        style="display:flex; flex-direction:column; align-items:flex-start; margin-right: auto;"
+      >
         <p>{contact.other_call}</p>
         <p>{contact.rst_sent}</p>
-    </div>
-  </div>
-  <div class="inline-section">
-    <p>Freq/Mode</p>
-    <div>
-        <p>{contact.freq_khz}kHz /</p>
-        <p>{contact.mode}</p>
-    </div>
+      </div>
 
+      <div>
+        <p style="color:brown; font-size:small;">recv'd</p>
+      </div>
+    </div>
   </div>
+  <div
+    style="display: flex;
+  width: 100%;
+  background: lightgray;"
+  >
+    <div
+      style="    display: flex;
+      gap: 4px;
+      width: 100%;
+      justify-content: center;
+      flex-direction: row;
+      align-items: center;"
+    >
+      <p style="font-size: x-small; word-wrap:normal; color: darkgray;">
+        Freq/Mode
+      </p>
+      <p>{contact.freq_khz}kHz/{contact.mode}</p>
+    </div>
+  </div>
+  <div
+    style="width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: lightgray;"
+  >
+    <p style="font-size: small; color:darkgray">memo</p>
+    <textarea
+      disabled
+      rows="5"
+      style="line-height: 1.1; width:95%; resize:none;">{contact.memo}</textarea
+    >
+  </div>
+  <div></div>
 </div>
 
 <style>
@@ -99,16 +172,6 @@ gap: 4px"
     padding-left: 5px;
     padding-right: 5px;
     width: 95%;
-  }
-  .field-name {
-    min-width: 60px;
-  }
-
-  .field-value {
-    overflow: hidden;
-    word-wrap: break-word;
-    overflow-wrap: break-word;
-    word-break: break-all;
   }
 
   b {
